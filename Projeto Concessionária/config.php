ffa73079dbc66fbd3e528d179bdd4206a938722b
Rafaella Bim <rafaella.bim@gmail.com>
2025-11-27 11:12:44 -0300

    Projeto

---
+++ Projeto Concessionária/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    define("HOST", "localhost");
+    define("USER", "root");
+    define("PASS", "");
+    define("BASE", "concessionaria2122n");
+
+    $conn = new MYSQLi(HOST, USER, PASS, BASE);
